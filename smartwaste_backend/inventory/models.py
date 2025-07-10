2f01e317d2b5a6a618dcf8b0e701cfeb7e3a2132
Khaled Al Daqrouq <aldaqrouqkhaled@gmail.com>
2025-07-10 17:31:05 +0400

    added a database model

--- smartwaste_backend/inventory/models.py
+++ smartwaste_backend/inventory/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    pass
+
+class InventoryItem(models.Model):
+    AVAILABLE = 'AV'
+    USED = 'US'
+    EXPIRED = "EX"
+    THROWN = "TH"
+    STATUS_CHOICES = {
+        USED: 'Used',
+        EXPIRED: 'Expired',
+        THROWN: 'Thrown',
+    }
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=100)
+    expiry_date = models.DateField()
+    quantity = models.PositiveIntegerField()
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+        default=AVAILABLE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
